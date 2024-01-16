dfecca492cf5cccec74b71cc235437becac5a29b
Руслан Габисов <rusik_gabisov@Ruslans-MacBook-Air.local>
2024-01-17 00:36:09 +0300

    Echo

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import {FormEvent, SyntheticEvent, useEffect, useState} from 'react'
+import './App.css'
+
+const URL_OF_CONNECTION = 'wss://echo.websocket.org';
+
+function App() {
+    const [webSocket, setWebSocket] = useState<WebSocket | null>(null);
+    const [inputValue, setInputValue] = useState<string>('');
+    useEffect(() => {
+        const webSocketClient = new WebSocket(URL_OF_CONNECTION);
+        webSocketClient.onopen = (event: Event) => {
+            console.log("WebSocket loaded", event);
+            setWebSocket(webSocketClient);
+        }
+        webSocketClient.onmessage = (data: MessageEvent) => {
+            console.log("Received ws message: ", data.data);
+        }
+        webSocketClient.onclose = () => {
+            console.log("WebSocket connection closed");
+        }
+    }, []);
+
+    function echoHandler(event: SyntheticEvent) {
+        event.preventDefault();
+        webSocket?.send(inputValue);
+    }
+
+    return (
+        <>
+            <form onSubmit={echoHandler}>
+                <input type="text" required onInput={(e: FormEvent<HTMLElement>) => {
+                    console.log("the vent is",);
+                    setInputValue(e.target?.value || '')
+                }}/>
+                <button>Echo</button>
+            </form>
+        </>
+    )
+}
+
+export default App
